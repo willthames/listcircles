e9f1bfed4e7a06afe0801c5f20ca9a93e581e7d5
Will Thames <will@musc.org.uk>
2011-08-07 18:36:11 +0100

    Added rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Listcircles::Application.config.secret_token = '5ae2681851ff861371f88b03fc329a9bc5c9517205bcceeb480e92d3a3d4fcf234d313e37eb553440d68bc55cba42e49e217bb13b62145b7ea631a33e6628bf0'
